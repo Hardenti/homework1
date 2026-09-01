c5268488aadf3e76b83c34b90060e87f49c2e624
Tiyana <tharden6@smartweb.augustatech.edu>
2026-08-31 23:20:15 -0400

    Update product_details for exercises 3 and 4

--- product_details.sql
+++ product_details.sql
@@ -1,3 +1,15 @@
+-- Exercise 3
 SELECT product_name, list_price, date_added
-FROM products
-ORDER BY product_name
+FROM Products
+WHERE list_price > 500 AND list_price < 2000
+ORDER BY date_added DESC;
+
+-- Exercise 4
+SELECT product_name,
+	   list_price,
+	   discount_percent,
+	   ROUND(list_price * (discount_percent / 100), 2) AS discount_amount,
+	   ROUND(list_price - (list_price * (discount_percent / 100)), 2) AS discount_price
+FROM Products
+ORDER BY discount_price DESC
+LIMIT 5;
